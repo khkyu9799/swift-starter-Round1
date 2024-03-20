92b94ff7c711ce0cdf4b89831947b84ba0131ba1
khkyu9799 <khkyu9799@naver.com>
2024-03-20 12:52:10 +0900

    Update iceCream.swift

--- iceCream.swift
+++ iceCream.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeIcecream(width: Int,height: Int)
+{
+    for _ in 1...height
+    {
+        for _ in 1...width
+        {
+            print("*",terminator: "")
+        }
+        print()
+    }
+}
 
+var width = 11
+var height = 8
+var blank = (width/2)-1
+
+makeIcecream(width: width, height: height)
+
+func makeHandler(height: Int)
+{
+    for _ in 1...height
+    {
+        for i in 1...blank
+        {
+            print("",terminator: " ")
+        }
+        print("ㅣ ㅣ")
+    }
+}
+
+makeHandler(height: height/2)
